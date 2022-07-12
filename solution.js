42be62f4415a899cdf4c84d111376af443d9a89d
Danica Yener <ny21037@googlemail.com>
2022-07-12 11:58:55 +0200

    fizzbuzz went over it

--- solution.js
+++ solution.js
@@ -10,11 +10,11 @@
 //     }
 // }
 let num = 5
-if (num % 3 == 0) {
+if (num % 3 == 0 && num % 5 !== 0) {
     console.log("Fizz");
-  } else if (num % 5 == 0 ) {
+  } else if (num % 5 == 0 && num % 3 !== 0 ) {
     console.log("Buzz");
-  } else if (num % 3 && 5 == 0){
+  } else if (num % 3 == 0 && num % 5 == 0){
     console.log("FizzBuzz");
   }else {
     console.log(num);
